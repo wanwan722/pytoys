b9391a51465690e600d3e38f3e1bb3e9393cdcf9
wanwan722 <wanwan722@gmail.com>
2016-04-07 19:02:47 +0800

    refactory the code; using VarType

--- src/parse_strut.py
+++ src/parse_strut.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
 import sys
-from shlex import *
 
-def lexer(input_file):
-  return list(shlex(open(input_file)))
+template_header = """\
+#include <string>
+#include <array>
 
-VAR_TYPE_INT, VAR_TYPE_DOUBLE, VAR_TYPE_CHAR = range(3)
+%s
+struct FieldDef
+{
+  std::string name;
+  VarType type;
+  unsigned int size;
+  unsigned int offset;
+};
+#define FIELD_DEFINE(c, n, t, s) {#n, t, s, offsetof(c, n)}
+
+"""
+
+class CVar(object):
+  def __init__(self, type, name, size=0):
+    self.type = type
+    self.name = name
+    self.size = size
+
+class CStruct(object):
+  def __init__(self, name):
+    self.name = name
+    self.vars = []
 
 class UnknownToken(Exception):
   def __init__(self, token):
@@ -14,16 +35,29 @@ class UnknownToken(Exception):
     self.token = token
     super(UnknownToken, self).__init__(msg)
 
-class Variable(object):
-  def __init__(self, type, name, size=0):
-    self.type = type
-    self.name = name
-    self.size = size
+class VarType(object):
+  """Define the C variable type this support"""
+  
+  Int, Float, Double, Char = range(4)
+  def __init__(self):
+    super(VarType, self).__init__()
 
-class Structure(object):
-  def __init__(self, name):
-    self.name = name
-    self.vars = []
+  @staticmethod
+  def token_to_type(token):
+    table = {"int":VarType.Int, "float":VarType.Float, "double":VarType.Double, "char":VarType.Char}
+    if token not in table:
+      raise UnknownToken(token)
+    return table[token]
+
+  @staticmethod
+  def type_to_enum(type):
+    table = ["VarType::Int", "VarType::Float", "VarType::Double", "VarType::Char"]
+    return table[type]
+
+  @staticmethod
+  def get_enum_def():
+    table = ["Int", "Float", "Double", "Char"]
+    return "enum class VarType { %s };" % (", ".join(table))
 
 class Grammer(object):
   STATE_SCOPE_FILE, STATE_SCOPE_STRUCTURE = range(2)
@@ -35,7 +69,6 @@ class Grammer(object):
     if self.__is_done():
       raise ValueError('EOL')
     token = self.__tokens[self.__index]
-    #print "Token: {0}".format(token)
     self.__index += 1
     return token
 
@@ -47,21 +80,16 @@ class Grammer(object):
   def __is_done(self):
     return self.__index == len(lexers) - 1
 
-  def __token2var(self, token):
-    table = {"int":(VAR_TYPE_INT, 8), "double":(VAR_TYPE_DOUBLE, 8), "char":(VAR_TYPE_CHAR, 0)}
-    if token not in table:
-      raise UnknownToken(token)
-    return table[token]
-
   def __parse_var(self, token):
-    var, size = self.__token2var(token)
+    var = VarType.token_to_type(token)
+    size = 0
     name = self.__consume_token()
-    if var == VAR_TYPE_CHAR:
+    if var == VarType.Char:
       self.__verify_next_token("[")
       size = int(self.__consume_token())
       self.__verify_next_token("]")
     self.__verify_next_token(";")
-    return Variable(var, name, size)
+    return CVar(var, name, size)
 
   def parse(self, lexers):
     self.__reset_state(lexers)
@@ -71,7 +99,7 @@ class Grammer(object):
       if self.__state == Grammer.STATE_SCOPE_FILE:
         if token == "struct":
           name = self.__consume_token()
-          structure = Structure(name)
+          structure = CStruct(name)
           self.__verify_next_token("{")
           self.__state = Grammer.STATE_SCOPE_STRUCTURE
         else:
@@ -82,7 +110,8 @@ class Grammer(object):
           self.__state = Grammer.STATE_SCOPE_FILE
           structure = None
         else:
-          structure.vars.append(self.__parse_var(token))
+          var = self.__parse_var(token)
+          structure.vars.append(var)
 
   def __reset_state(self, lexers):
     self.__state = Grammer.STATE_SCOPE_FILE
@@ -90,32 +119,17 @@ class Grammer(object):
     self.__tokens = lexers
     self.structures = []
 
+def lexer(input_file):
+  import shlex
+  return list(shlex.shlex(open(input_file)))
+
 def output_struct_def(structures, filename):
-  def __to_type(type):
-    table = {VAR_TYPE_INT:"VarType::Int", VAR_TYPE_DOUBLE:"VarType::Double", VAR_TYPE_CHAR:"VarType::Char"}
-    return table[type]
-
   with open(filename, "w") as f:
-    f.write("""\
-#include <string>
-#include <array>
-
-enum class VarType { Int, Double, Char };
-struct FieldDef
-{
-  std::string name;
-  VarType type;
-  unsigned int size;
-  unsigned int offset;
-};
-#define FIELD_DEFINE(c, n, t, s) {#n, t, s, offsetof(c, n)}
-
-""")
-
+    f.write(template_header % VarType.get_enum_def())
     for s in structures:
-      f.write("const std::array<FieldDef, {1}> {0}_Fields = {{{{\n".format(s.name, len(s.vars)))
+      f.write("const std::array<FieldDef, %d> %s_Fields = {{\n" % (len(s.vars), s.name))
       for v in s.vars:
-        f.write("\tFIELD_DEFINE({0}, {1}, {2}, {3}),\n".format(s.name, v.name, __to_type(v.type), v.size))
+        f.write("\tFIELD_DEFINE(%s, %s, %s, %d),\n" % (s.name, v.name, VarType.type_to_enum(v.type), v.size))
       f.write("}};\n")
 
 if __name__ == "__main__":
@@ -123,9 +137,6 @@ if __name__ == "__main__":
     print "USAGE: {0} src_file fields_file".format(sys.argv[0])
     sys.exit(0)
 
-  try:
-    lexers = lexer(sys.argv[1])
-    gramer = Grammer(lexers)
-    output_struct_def(gramer.structures, sys.argv[2])
-  except Exception as e:
-    print e
+  lexers = lexer(sys.argv[1])
+  gramer = Grammer(lexers)
+  output_struct_def(gramer.structures, sys.argv[2])
